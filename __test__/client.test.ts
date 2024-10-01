4ec7b578e16ab42290e4933f3a1039ca7eaf9541
Aleksei Gerasev <alexey@concero.io>
2024-10-01 20:43:22 +0300

    add tests for ConceroClient

---
+++ __test__/client.test.ts
@@ -0,0 +1,77 @@
+import { ConceroClient } from "../src/core/client";
+
+describe("ConceroClient", () => {
+    let client: ConceroClient;
+    beforeEach(() => {
+        client = new ConceroClient(0, "test");
+    })
+
+    describe("getRoute", () => {
+        it("getRoute", async () => {
+            const route = await client.getRoute({
+                fromChainId: "8453",
+                toChainId: "137",
+                fromToken: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
+                toToken: "0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359",
+                amount: "100000000",
+                slippageTolerance: "0.5",
+            });
+            expect(route).toBeDefined();
+            //console.log(route);
+        });
+
+        it("getRoute fails", async () => {
+            const unsupportedChainId = "9999";
+            const route = await client.getRoute({
+                fromChainId: unsupportedChainId,
+                toChainId: "137",
+                fromToken: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
+                toToken: "0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359",
+                amount: "1",
+                slippageTolerance: "0.5",
+            });
+            expect(route).toBeUndefined();
+            //console.log(route);
+        });
+    });
+
+    describe("getTokens", () => {
+        it("getTokens with chainId", async () => {
+            const tokens = await client.getSupportedTokens({
+                chainId: "137"
+            });
+            expect(tokens).toBeDefined();
+            expect(tokens?.length).toBeGreaterThan(0);
+            //console.log(tokens);
+        });
+
+        it("getTokens with symbol", async () => {
+            const tokens = await client.getSupportedTokens({
+                chainId: "137", symbol: "ETH"
+            });
+            expect(tokens).toBeDefined();
+            expect(tokens?.length).toBeGreaterThan(0);
+            //console.log(tokens);
+        });
+
+        it("getTokens with name", async () => {
+            const tokens = await client.getSupportedTokens({
+                chainId: "137", name: "USD Coin"
+            });
+            expect(tokens).toBeDefined();
+            expect(tokens?.length).toBeGreaterThan(0);
+            //console.log(tokens);
+        });
+    });
+
+
+    describe("getChains", () => {
+        it("getChains", async () => {
+            const chains = await client.getSupportedChains();
+            expect(chains).toBeDefined();
+            expect(chains?.length).toBeGreaterThan(0);
+            //console.log(chains);
+        });
+    });
+
+})
