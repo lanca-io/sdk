657ee59efc42b5c4c80f822170606f8dd48f2f22
Aleksei Gerasev <alexey@concero.io>
2024-10-02 10:44:21 +0300

    update configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,10 +2,15 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+	{ files: ["**/*.{js,mjs,cjs,ts}"] },
+	{
+		languageOptions: { globals: { ...globals.browser, ...globals.node } },
+		rules: {
+			"@typescript-eslint/no-unused-vars": "warn",
+			"@typescript-eslint/no-explicit-any": "warn",
+		},
+	},
+	pluginJs.configs.recommended,
+	...tseslint.configs.recommended,
 ];
