a2beb4e106040f1ae56b07502606defcce4bdcf1
Aleksei Gerasev <alexey@concero.io>
2024-10-03 11:07:44 +0300

    add uniV3 router addresses

---
+++ src/constants/uniswapV3RouterAddressesMap.ts
@@ -0,0 +1,9 @@
+import { type Address } from "viem";
+
+export const uniswapV3RouterAddressesMap: Record<string, Address> = {
+	"1": "0xE592427A0AEce92De3Edee1F18E0157C05861564",
+	"137": "0xE592427A0AEce92De3Edee1F18E0157C05861564",
+	"42161": "0xE592427A0AEce92De3Edee1F18E0157C05861564",
+	"43114": "0xbb00FF08d01D300023C629E8fFfFcb65A5a578cE",
+	"8453": "0x2626664c2603336E57B271c5C0b26F421741e481",
+};
