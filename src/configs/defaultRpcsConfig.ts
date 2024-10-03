6f6f4bf721a9f4860519fe275eb3cb9b1e620f5d
Aleksei Gerasev <alexey@concero.io>
2024-10-03 11:04:23 +0300

    add constants

---
+++ src/configs/defaultRpcsConfig.ts
@@ -0,0 +1,28 @@
+import { fallback, http } from "viem";
+import { base, polygon, arbitrum, avalanche } from "viem/chains";
+
+export const defaultRpcsConfig = {
+	[polygon.id]: fallback([
+		http(),
+		http("https://polygon.meowrpc.com"),
+		http("https://polygon-pokt.nodies.app"),
+		http("https://polygon-bor-rpc.publicnode.com"),
+	]),
+	[arbitrum.id]: fallback([
+		http(),
+		http("https://1rpc.io/arb"),
+		http("https://endpoints.omniatech.io/v1/arbitrum/one/public"),
+		http("https://arbitrum.meowrpc.com"),
+	]),
+	[avalanche.id]: fallback([
+		http(),
+		http("https://avalanche-c-chain-rpc.publicnode.com"),
+		http("https://avalanche.drpc.org"),
+		http("https://1rpc.io/avax/c"),
+	]),
+	[base.id]: fallback([
+		http("https://base-rpc.publicnode.com"),
+		http("https://base.drpc.org"),
+		http("https://1rpc.io/base"),
+	]),
+};
---
+++ src/constants/dexTypesMap.ts
@@ -0,0 +1,6 @@
+export const dexTypesMap: Record<string, number> = {
+	uniswapV3Single: 3,
+	uniswapV3Multi: 5,
+	wrapNative: 9,
+	unwrapWNative: 10,
+};
