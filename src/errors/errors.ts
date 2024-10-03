a70dd4f32b8eccb2bab562a5a023d036db356395
Aleksei Gerasev <alexey@concero.io>
2024-10-03 11:02:57 +0300

    fixed typos and import

--- src/errors/errors.ts
+++ src/errors/errors.ts
@@ -1,17 +1,62 @@
-export class UnsupportedToken extends Error {
-    /**
-     * @param token The unsupported token
-     */
-    constructor(token: string) {
-        super(`Unsupported token: ${token}`);
-    }
+export class UnsupportedTokenError extends Error {
+	/**
+	 * @param token The unsupported token
+	 */
+	constructor(token: string) {
+		super(`Unsupported token: ${token}`);
+	}
 }
 
-export class UnsupportedChain extends Error {
-    /**
-     * @param chainId The unsupported chain
-     */
-    constructor(chainId: string) {
-        super(`Unsupported chain: ${chainId}`);
-    }
+export class UnsupportedChainError extends Error {
+	/**
+	 * @param chainId The unsupported chain
+	 */
+	constructor(chainId: string) {
+		super(`Unsupported chain: ${chainId}`);
+	}
+}
+
+export class EmptyAmountError extends Error {
+	/**
+	 * @param amount The empty amount
+	 */
+	constructor(amount: string) {
+		super(`Empty amount: ${amount}`);
+	}
+}
+
+export class TokensAreTheSameError extends Error {
+	/**
+	 * @param token The same token
+	 */
+	constructor(token: string) {
+		super(`Tokens are the same: ${token}`);
+	}
+}
+
+export class WalletClientError extends Error {
+	/**
+	 * @param error The wallet client error
+	 */
+	constructor(error: string) {
+		super(`Wallet client error: ${error}`);
+	}
+}
+
+export class PublicClientError extends Error {
+	/**
+	 * @param error The public client error
+	 */
+	constructor(error: string) {
+		super(`Public client error: ${error}`);
+	}
+}
+
+export class RouteError extends Error {
+	/**
+	 * @param error The route error
+	 */
+	constructor(error: string) {
+		super(`Route error: ${error}`);
+	}
 }
