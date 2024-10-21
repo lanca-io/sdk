5b9773ea498fe33fa3b79951be89972b96c11880
Aleksei Gerasev <alexey@concero.io>
2024-10-21 12:31:30 +0300

    delete comments

--- src/core/checkAllowanceAndApprove.ts
+++ src/core/checkAllowanceAndApprove.ts
@@ -1,116 +1,56 @@
-import {
-	Address,
-	erc20Abi,
-	parseUnits,
-	PublicClient,
-	WalletClient,
-	zeroAddress,
-} from "viem";
-import { SwapDirectionData } from "../types/tx";
-import { conceroAddressesMap } from "../configs";
-import { ExecuteRouteInfo, ExecuteRouteStage, ExecuteRouteStatus, UpdateRouteHook } from "../types";
+import { Address, erc20Abi, parseUnits, PublicClient, WalletClient, zeroAddress } from 'viem'
+import { SwapDirectionData } from '../types/tx'
+import { conceroAddressesMap } from '../configs'
+import { ExecuteRouteStatus, UpdateRouteHook } from '../types'
 
 export async function checkAllowanceAndApprove(
 	walletClient: WalletClient,
 	publicClient: PublicClient,
 	txData: SwapDirectionData,
 	clientAddress: Address,
-	updateStateHook: UpdateRouteHook
+	status: any,
+	updateRouteStatusHook?: UpdateRouteHook,
 ) {
-	const { token, amount, chain } = txData;
+	const { token, amount, chain } = txData
 	if (token.address === zeroAddress) {
-		return;
+		return
 	}
 
 	if (!chain) {
-		return;
+		return
 	}
 
-	const currentState: Array<ExecuteRouteInfo> = [
-		{
-			stage: ExecuteRouteStage.SwitchChain,
-			status: ExecuteRouteStatus.Success,
-		},
-		{
-			stage: ExecuteRouteStage.Allowance,
-			status: ExecuteRouteStatus.Pending,
-		},
-		{
-			stage: ExecuteRouteStage.Swap,
-			status: ExecuteRouteStatus.NotStarted,
-		},
-	];
-
-	{
-		switchChain: ExecuteRouteStatus.Success,
-		allowance: ExecuteRouteStatus.Pending,
-		swap: ExecuteRouteStatus.NotStarted,
-		txLink: null,
-	}
-
-	updateStateHook(currentState);
-
-	const conceroAddress = conceroAddressesMap[chain.id];
+	const conceroAddress = conceroAddressesMap[chain.id]
 	const allowance = await publicClient.readContract({
 		abi: erc20Abi,
-		functionName: "allowance",
+		functionName: 'allowance',
 		address: token.address as `0x${string}`,
 		args: [clientAddress, conceroAddress],
-	});
+	})
 
-	let approveTxHash = null;
-	const amountInDecimals = parseUnits(amount, token.decimals);
-
-	currentState[1].status = ExecuteRouteStatus.Success;
-	updateStateHook(currentState);
-
+	let approveTxHash = null
+	const amountInDecimals = parseUnits(amount, token.decimals)
 
 	if (allowance < amountInDecimals) {
 		const { request } = await publicClient.simulateContract({
 			account: clientAddress,
 			address: token.address as `0x${string}`,
 			abi: erc20Abi,
-			functionName: "approve",
+			functionName: 'approve',
 			args: [conceroAddress, amountInDecimals],
-		});
+		})
 
-		approveTxHash = await walletClient.writeContract(request);
+		status.allowanceApprove = ExecuteRouteStatus.Pending
+		updateRouteStatusHook?.(status)
+
+		approveTxHash = await walletClient.writeContract(request)
 	}
 
 	if (approveTxHash) {
-		await publicClient.waitForTransactionReceipt({ hash: approveTxHash });
-		updateStateHook([
-			{
-				stage: ExecuteRouteStage.SwitchChain,
-				status: ExecuteRouteStatus.Success,
-			},
-			{
-				stage: ExecuteRouteStage.Allowance,
-				status: ExecuteRouteStatus.Success,
-			},
-			{
-				stage: ExecuteRouteStage.Swap,
-				status: ExecuteRouteStatus.NotStarted,
-			},
-		]);
+		await publicClient.waitForTransactionReceipt({ hash: approveTxHash })
+		status.allowanceApprove = ExecuteRouteStatus.Success
+	} else {
+		status.allowanceApprove = ExecuteRouteStatus.Failed
 	}
-	else {
-		updateStateHook([
-			{
-				stage: ExecuteRouteStage.SwitchChain,
-				status: ExecuteRouteStatus.Success,
-			},
-			{
-				stage: ExecuteRouteStage.Allowance,
-				status: ExecuteRouteStatus.Success,
-			},
-			{
-				stage: ExecuteRouteStage.Swap,
-				status: ExecuteRouteStatus.NotStarted,
-			},
-		]);
-	}
-
-
-
+	updateRouteStatusHook?.(status)
 }
