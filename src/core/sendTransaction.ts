c5907487cb6345d762c1a12d3414dd53a2cb3597
Aleksei Gerasev <alexey@concero.io>
2024-10-21 12:30:08 +0300

    format

--- src/core/sendTransaction.ts
+++ src/core/sendTransaction.ts
@@ -1,6 +1,6 @@
-import { Address, PublicClient, WalletClient, zeroAddress } from "viem";
-import { InputRouteData, SwapArgs, TxName } from "../types";
-import { conceroAbi } from "../abi";
+import { Address, PublicClient, WalletClient, zeroAddress } from 'viem'
+import { InputRouteData, SwapArgs, TxName } from '../types'
+import { conceroAbi } from '../abi'
 
 export async function sendTransaction(
 	txArgs: InputRouteData,
@@ -9,22 +9,21 @@ export async function sendTransaction(
 	conceroAddress: Address,
 	clientAddress: Address,
 ) {
-	const { srcSwapData, bridgeData, dstSwapData } = txArgs;
-	let txName: TxName = "swap";
-	let args: SwapArgs = [srcSwapData, clientAddress];
+	const { srcSwapData, bridgeData, dstSwapData } = txArgs
+	let txName: TxName = 'swap'
+	let args: SwapArgs = [srcSwapData, clientAddress]
 
 	if (srcSwapData.length > 0 && bridgeData) {
-		txName = "swapAndBridge";
-		args = [bridgeData, srcSwapData, dstSwapData];
+		txName = 'swapAndBridge'
+		args = [bridgeData, srcSwapData, dstSwapData]
 	}
 	if (srcSwapData.length === 0 && bridgeData) {
-		txName = "bridge";
-		args = [bridgeData, dstSwapData];
+		txName = 'bridge'
+		args = [bridgeData, dstSwapData]
 	}
 
-	const gasPrice = await publicClient.getGasPrice();
-	const isFromNativeToken =
-		srcSwapData.length > 0 && srcSwapData[0].fromToken === zeroAddress;
+	const gasPrice = await publicClient.getGasPrice()
+	const isFromNativeToken = srcSwapData.length > 0 && srcSwapData[0].fromToken === zeroAddress
 
 	return await walletClient.writeContract({
 		chain: null,
@@ -36,5 +35,5 @@ export async function sendTransaction(
 		gas: 3_000_000n,
 		gasPrice,
 		value: isFromNativeToken ? srcSwapData[0].fromAmount : undefined,
-	});
+	})
 }
--- src/types/contractInputTypes.ts
+++ src/types/contractInputTypes.ts
@@ -1,29 +1,29 @@
-import { type Address } from "viem";
+import { type Address } from 'viem'
 
 export interface BridgeData {
-	tokenType: number;
-	amount: bigint;
-	dstChainSelector: bigint;
-	receiver: Address;
+	tokenType: number
+	amount: bigint
+	dstChainSelector: bigint
+	receiver: Address
 }
 
 export interface InputSwapData {
-	dexType?: number;
-	tokenType?: number;
-	fromToken: Address;
-	fromAmount: bigint;
-	toToken: Address;
-	toAmount: bigint;
-	toAmountMin: bigint;
-	dexData?: any;
+	dexType?: number
+	tokenType?: number
+	fromToken: Address
+	fromAmount: bigint
+	toToken: Address
+	toAmount: bigint
+	toAmountMin: bigint
+	dexData?: any
 }
 
-export type TxName = "swap" | "bridge" | "swapAndBridge";
+export type TxName = 'swap' | 'bridge' | 'swapAndBridge'
 
 export interface InputRouteData {
-	bridgeData: BridgeData | null;
-	srcSwapData: InputSwapData[];
-	dstSwapData: InputSwapData[];
+	bridgeData: BridgeData | null
+	srcSwapData: InputSwapData[]
+	dstSwapData: InputSwapData[]
 }
 
-export type SwapArgs = Array<InputSwapData[] | BridgeData | bigint | Address>;
+export type SwapArgs = Array<InputSwapData[] | BridgeData | bigint | Address>
