bfa2d097773ee57a405015a3f2ca752a283d1272
Aleksei Gerasev <alexey@concero.io>
2024-11-09 15:03:53 +0300

    delete empty txHash

--- src/core/checkAllowanceAndApprove.ts
+++ src/core/checkAllowanceAndApprove.ts
@@ -56,7 +56,6 @@ export async function checkAllowanceAndApprove(
 	} else {
 		routeStatus.approveAllowance = {
 			status: Status.FAILED,
-			txHash: '',
 			error: 'Failed to approve allowance'
 		}
 	}
