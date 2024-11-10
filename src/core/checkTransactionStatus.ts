12a1783d1ff5fb348b7a8fba721e42bc022de1b1
Aleksei Gerasev <alexey@concero.io>
2024-11-10 11:54:04 +0300

    replace setInterval by loop

--- src/core/checkTransactionStatus.ts
+++ src/core/checkTransactionStatus.ts
@@ -3,6 +3,7 @@ import { Status, TxStep, UpdateRouteHook } from '../types'
 import { RouteTypeExtended } from '../types'
 import { baseUrl } from '../constants'
 import { viemReceiptConfig } from '../constants'
+import { sleep } from '../utils/sleep'
 
 export async function checkTransactionStatus(
 	txHash: Address,
@@ -46,27 +47,12 @@ export async function checkTransactionStatus(
 	//@review-from-oleg – if status is null, we should handle this
 
 	const timeInterval = 3000
-	let isDone = false //@review-from-oleg - rename to isTransactionComplete for clarity
+	let isTransactionComplete = false //@review-from-oleg - rename to isTransactionComplete for clarity
 
 	//@review-from-oleg - This part needs to be re-written.
 	// if the fetch takes 4 seconds, you would still send a fetch request every 3 seconds without awaiting it
 	// rewrite using while(shouldFetch) and sleep(fetchIntervalMS). Don't use setInterval or setTimeout
-	const intervalId = setInterval(async () => {
-		if (isDone) {
-			clearInterval(intervalId)
-			updateRouteStatusHook?.({
-				...routeStatus,
-				steps: routeStatus.steps.map(step => ({
-					...step,
-					execution: {
-						status: Status.SUCCESS,
-						txHash,
-					},
-				})),
-			})
-			return
-		}
-
+	while (!isTransactionComplete) {
 		try {
 			//	@review-from-oleg - we need to refactor fetch to use a custom wrapper 'request.ts' which will add custom headers and handle errors, so we don't duplicate this 200 check everywhere
 			const response = await fetch(`${baseUrl}/route_status?txHash=${txHash}`)
@@ -77,12 +63,25 @@ export async function checkTransactionStatus(
 
 			const steps: TxStep[] = await response.json()
 			if (steps.every(({ status }) => status === Status.SUCCESS)) {
-				isDone = true
+				isTransactionComplete = true
 			}
+			sleep(timeInterval)
 		} catch (error) {
 			//@review-from-oleg – we need a custom, global error handler. This should not only catch all errors, but also send them as reports to our API.
 			//also everywhere where you're logging errors please use console.error	instead of console.log
 			console.log(error)
 		}
-	}, timeInterval)
+	}
+
+
+	updateRouteStatusHook?.({
+		...routeStatus,
+		steps: routeStatus.steps.map(step => ({
+			...step,
+			execution: {
+				status: Status.SUCCESS,
+				txHash,
+			},
+		})),
+	})
 }
