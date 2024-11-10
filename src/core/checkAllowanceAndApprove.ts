fa3f4978d8a532a734d8ff9e414295777e7d0227
Aleksei Gerasev <alexey@concero.io>
2024-11-10 21:50:44 +0300

    refactor and adapt for the new RouteType

--- src/core/checkAllowanceAndApprove.ts
+++ src/core/checkAllowanceAndApprove.ts
@@ -1,5 +1,5 @@
-import { Address, erc20Abi, parseUnits, PublicClient, WalletClient, zeroAddress } from 'viem'
-import { Status, SwapDirectionData } from '../types/tx'
+import { Address, erc20Abi, parseUnits, PublicClient, WalletClient } from 'viem'
+import { Status, StepType, SwapDirectionData } from '../types'
 import { conceroAddressesMap } from '../configs'
 import { RouteType, UpdateRouteHook } from '../types'
 import { isNative } from '../utils'
@@ -14,7 +14,6 @@ export async function checkAllowanceAndApprove(
 ) {
 	const { token, amount, chain } = txData
 	if (isNative(token.address)) {
-		routeStatus.approveAllowance.status = Status.SUCCESS
 		return
 	}
 
@@ -28,10 +27,20 @@ export async function checkAllowanceAndApprove(
 
 	const amountInDecimals: bigint = parseUnits(amount, token.decimals)
 
-	if (allowance >= amountInDecimals) {
-		routeStatus.approveAllowance = {
-			status: Status.SUCCESS,
+	const isSwitchStepPresent = routeStatus.steps[0].type === StepType.SWITCH_CHAIN
+	const allowanceIndex = isSwitchStepPresent ? 1 : 0
+
+	routeStatus.steps.splice(allowanceIndex, 0, {
+		type: StepType.ALLOWANCE,
+		execution: {
+			status: Status.NOT_STARTED
 		}
+	})
+
+	const { execution } = routeStatus.steps[allowanceIndex]
+
+	if (allowance >= amountInDecimals) {
+		execution.status = Status.SUCCESS
 		updateRouteStatusHook?.(routeStatus)
 		return
 	}
@@ -44,28 +53,22 @@ export async function checkAllowanceAndApprove(
 		args: [conceroAddress, amountInDecimals],
 	})
 
-	routeStatus.approveAllowance.status = Status.PENDING
+	execution.status = Status.PENDING
 	updateRouteStatusHook?.(routeStatus)
 
 	try {
 		const approveTxHash = await walletClient.writeContract(request)
 		if (approveTxHash) {
 			await publicClient.waitForTransactionReceipt({ hash: approveTxHash })
-			routeStatus.approveAllowance = {
-				status: Status.SUCCESS,
-				txHash: approveTxHash,
-			}
+			execution.status = Status.SUCCESS
+			execution.txHash = approveTxHash
 		} else {
-			routeStatus.approveAllowance = {
-				status: Status.FAILED,
-				error: 'Failed to approve allowance'
-			}
+			execution.status = Status.FAILED
+			execution.error = 'Failed to approve allowance'
 		}
 	} catch (error) {
-		routeStatus.approveAllowance = {
-			status: Status.FAILED,
-			error: 'Failed to approve allowance'
-		}
+		execution.status = Status.FAILED
+		execution.error = 'Failed to approve allowance'
 	}
 
 	updateRouteStatusHook?.(routeStatus)
