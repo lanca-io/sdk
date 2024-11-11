a74a2c3b65d6eef47991edcec0884c6e085abdca
Aleksei Gerasev <alexey@concero.io>
2024-11-11 17:53:00 +0300

    add conceroApi config

---
+++ src/configs/apis.ts
@@ -0,0 +1,8 @@
+import { baseUrl } from "../constants";
+
+export const conceroApi = {
+    routeStatus: `${baseUrl}/route_status`,
+    route: `${baseUrl}/route`,
+    tokens: `${baseUrl}/tokens`,
+    chains: `${baseUrl}/chains`,
+} 
