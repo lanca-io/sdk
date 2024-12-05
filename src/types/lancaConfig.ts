e134a8e6530bb72a8bcce5f788ba5b107df55222
Aleksei Gerasev <alexey@concero.io>
2024-12-05 18:24:26 +0300

    rename

--- src/types/lancaConfig.ts
+++ src/types/lancaConfig.ts
@@ -1,8 +1,8 @@
-import { FallbackTransport } from 'viem'
+import { Address, FallbackTransport } from 'viem'
 
 export type RpcConfig = Record<number, string[]> | Record<number, FallbackTransport>
 export interface LancaSDKConfig {
-	integratorId: string
-	feeTier: number
+	integratorAddress: Address
+	feeBps: number
 	chains?: RpcConfig
 }
