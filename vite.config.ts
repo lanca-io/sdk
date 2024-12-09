8c765732e21e0f91ef21c572fa0677c8cafeead9
Aleksei Gerasev <alexey@concero.io>
2024-12-09 13:12:18 +0300

    change output

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
 		lib: {
 			entry: resolve(__dirname, 'src/index.ts'),
 			name: '@lanca/sdk',
-			fileName: (format) => `sdk.${format}.js`,
+			fileName: (format) => `index.${format}.js`,
 		},
 		rollupOptions: {
 			external: ['pino', 'viem', 'solady'],
