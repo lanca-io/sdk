44dcd064d9c34f9e060c0e1fee0e3493276d936a
Aleksei Gerasev <alexey@concero.io>
2024-12-10 18:11:53 +0300

    clean tests

--- __test__/client.test.ts
+++ __test__/client.test.ts
@@ -1,16 +1,21 @@
 import { createWalletClient, http } from 'viem'
-import { LansaSDK } from '../src/core/client'
-import { base } from 'viem/chains'
 import { privateKeyToAccount } from 'viem/accounts'
+import { base } from 'viem/chains'
+import { DEFAULT_SLIPPAGE } from '../src/constants'
+import { LansaSDK } from '../src/core/client'
 
 // @review add execute route test (swap, bridge, swapAndBridge)
+
+const TEST_TIMEOUT = 100_000
+const FROM_ADDRESS = '0x8335Af2c71e1B39f75ccFb5389211A2A78a3EE03'
+const TO_ADDRESS = '0x8335Af2c71e1B39f75ccFb5389211A2A78a3EE03'
 
 describe('ConceroClient', () => {
 	let client: LansaSDK
 	beforeEach(() => {
 		client = new LansaSDK({
-			integratorId: '1',
-			feeTier: 1000,
+			integratorAddress: FROM_ADDRESS,
+			feeBps: 1,
 			chains: {
 				'8453': ['https://rpc.ankr.com/eth'],
 				'137': ['https://polygon-rpc.com'],
@@ -18,17 +23,18 @@ describe('ConceroClient', () => {
 		})
 	})
 
-
 	describe('executeRoute', () => {
 		let route, walletClient, account
 		beforeEach(async () => {
 			route = await client.getRoute({
 				fromChainId: '8453',
 				toChainId: '8453',
-				fromToken: '0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913',//USDC
-				toToken: '0x50c5725949A6F0c72E6C4a641F24049A917DB0Cb',//DAI
+				fromToken: '0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913', //USDC
+				toToken: '0x50c5725949A6F0c72E6C4a641F24049A917DB0Cb', //DAI
 				amount: '1',
-				slippageTolerance: '0.5',
+				fromAddress: FROM_ADDRESS,
+				toAddress: TO_ADDRESS,
+				slippageTolerance: DEFAULT_SLIPPAGE,
 			})
 
 			account = privateKeyToAccount(process.env.PRIVATE_KEY)
@@ -37,39 +43,47 @@ describe('ConceroClient', () => {
 				chain: base,
 				transport: http(),
 			})
+		}, TEST_TIMEOUT)
 
+		it(
+			'test_canSwapSingleChain',
+			async () => {
+				console.log('route', route)
+				const routeWithStatus = await client.executeRoute(route, walletClient, {
+					switchChainHook: (chainId: number) => {
+						console.log('switchChainHook chainId', chainId)
+					},
+					updateRouteStatusHook: routeStatus => {
+						console.log(routeStatus)
+					},
+				})
 
-		})
-		it('test_canSwapSingleChain', async () => {
-			console.log('route', route)
-			const routeWithStatus = await client.executeRoute(route, walletClient, {
-				switchChainHook: (chainId: number) => {
-					console.log('switchChainHook chainId', chainId)
-				},
-				updateRouteStatusHook: (routeStatus) => {
-					console.log(routeStatus)
-				}
-			})
-
-			const routeStatus = await client.getRouteStatus(txHash)
-			console.log(routeStatus)
-		})
+				const routeStatus = await client.getRouteStatus(txHash)
+				console.log(routeStatus)
+				expect(routeStatus).toBeDefined()
+			},
+			TEST_TIMEOUT,
+		)
 	})
 
-
 	describe('getRoute', () => {
-		it('test_canGetRoute', async () => {
-			const route = await client.getRoute({
-				fromChainId: '8453',
-				toChainId: '137',
-				fromToken: '0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913',
-				toToken: '0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359',
-				amount: '100000000',
-				slippageTolerance: '0.5',
-			})
-			expect(route).toBeDefined()
-			//console.log(route);
-		})
+		it(
+			'test_canGetRoute',
+			async () => {
+				const route = await client.getRoute({
+					fromChainId: '8453',
+					toChainId: '137',
+					fromToken: '0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913',
+					toToken: '0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359',
+					amount: '100000000',
+					fromAddress: FROM_ADDRESS,
+					toAddress: TO_ADDRESS,
+					slippageTolerance: DEFAULT_SLIPPAGE,
+				})
+				expect(route).toBeDefined()
+			},
+			TEST_TIMEOUT,
+		)
 
 		it('test_failsWithUnsupportedChain', async () => {
 			const unsupportedChainId = '9999'
@@ -79,7 +93,9 @@ describe('ConceroClient', () => {
 				fromToken: '0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913',
 				toToken: '0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359',
 				amount: '1',
-				slippageTolerance: '0.5',
+				fromAddress: FROM_ADDRESS,
+				toAddress: TO_ADDRESS,
+				slippageTolerance: DEFAULT_SLIPPAGE,
 			})
 			expect(route).toBeUndefined()
 		})
@@ -93,7 +109,9 @@ describe('ConceroClient', () => {
 				fromToken: unsupportedTokenFrom,
 				toToken: unsupportedTokenTo,
 				amount: '1',
-				slippageTolerance: '0.5',
+				fromAddress: FROM_ADDRESS,
+				toAddress: TO_ADDRESS,
+				slippageTolerance: DEFAULT_SLIPPAGE,
 			})
 			expect(route).toBeUndefined()
 		})
@@ -106,7 +124,6 @@ describe('ConceroClient', () => {
 			})
 			expect(tokens).toBeDefined()
 			expect(tokens?.length).toBeGreaterThan(0)
-			//console.log(tokens);
 		})
 
 		it('test_canGetTokensWithSymbol', async () => {
@@ -116,7 +133,6 @@ describe('ConceroClient', () => {
 			})
 			expect(tokens).toBeDefined()
 			expect(tokens?.length).toBeGreaterThan(0)
-			//console.log(tokens);
 		})
 
 		it('test_canGetTokensWithNameAndChainId', async () => {
@@ -126,7 +142,6 @@ describe('ConceroClient', () => {
 			})
 			expect(tokens).toBeDefined()
 			expect(tokens?.length).toBeGreaterThan(0)
-			//console.log(tokens);
 		})
 	})
 
@@ -135,7 +150,6 @@ describe('ConceroClient', () => {
 			const chains = await client.getSupportedChains()
 			expect(chains).toBeDefined()
 			expect(chains?.length).toBeGreaterThan(0)
-			//console.log(chains);
 		})
 	})
 })
