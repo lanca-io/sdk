688f1d39f8b2a1bf5fa536039966a937a415c72f
Aleksei Gerasev <alexey@concero.io>
2024-12-20 12:42:02 +0300

    remove unused imports

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,4 @@
 export * from './baseUrl'
-export * from './uniswapV3RouterAddressesMap'
-export * from './dexTypesMap'
 export * from './viemReceiptConfig'
 export * from './gas'
 export * from './default'
