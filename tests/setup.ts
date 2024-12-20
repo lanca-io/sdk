c64a64696890c4bd51bf982b4b3f23441770303f
Aleksei Gerasev <alexey@concero.io>
2024-12-21 01:37:52 +0300

    replace jest with vitest

---
+++ tests/setup.ts
@@ -0,0 +1,7 @@
+import { Address } from 'viem'
+
+export const FROM_ADDRESS = '0xa1c9e0381150Eb24d164Ef0Db18E606e65F751Af'.toLowerCase() as Address
+export const TO_ADDRESS = '0xf4eac421F8fA86E499b56848605E604e3ff6D22F'.toLowerCase() as Address
+export const USDC_BASE = '0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913'.toLowerCase() as Address
+export const USDC_POLYGON = '0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359'.toLowerCase() as Address
+export const TEST_TIMEOUT = 100_000
