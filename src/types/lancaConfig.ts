047fe6b28e313b9cc3fdc1c1329824d196f7df6c
Aleksei Gerasev <alexey@concero.io>
2024-12-20 13:33:29 +0300

    make all fields as optional

--- src/types/lancaConfig.ts
+++ src/types/lancaConfig.ts
@@ -2,7 +2,7 @@ import { Address, FallbackTransport } from 'viem'
 
 export type RpcConfig = Record<number, string[]> | Record<number, FallbackTransport>
 export interface LancaClientConfig {
-	integratorAddress: Address
-	feeBps: number
-	chains: RpcConfig
+	integratorAddress?: Address
+	feeBps?: number
+	chains?: RpcConfig
 }
