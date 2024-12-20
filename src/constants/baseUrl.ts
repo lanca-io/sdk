f5280ddb8bdc15db8a788d3b21c1c2aac8afb74c
Aleksei Gerasev <alexey@concero.io>
2024-12-21 00:13:13 +0300

    add .env support

--- src/constants/baseUrl.ts
+++ src/constants/baseUrl.ts
@@ -1,3 +1,3 @@
 const PRODUCTION_URL = 'https://api.concero.io/api'
 const LOCAL_URL = 'http://127.0.0.1:4000/api'
-export const BASE_URL = process.env.DEBUG ? LOCAL_URL : PRODUCTION_URL
+export const BASE_URL = import.meta.env.VITE_DEBUG ? LOCAL_URL : PRODUCTION_URL
