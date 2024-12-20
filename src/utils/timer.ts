f37ccc12d21a3a98af339e45fd2e0d9fca3c3fc7
Aleksei Gerasev <alexey@concero.io>
2024-12-20 12:42:08 +0300

    add docs

--- src/utils/timer.ts
+++ src/utils/timer.ts
@@ -1,3 +1,9 @@
+/**
+ * Timer function that calls the given function every second with an increasing counter argument.
+ *
+ * @param {function} func - The function to be called every second.
+ * @returns {function} A function that can be called to stop the timer.
+ */
 export const timer = (func: (num: number) => void) => {
 	let counterTime = 1
 
