a1e4289b379151487229bf44da069d1a27dcbe33
Aleksei Gerasev <alexey@concero.io>
2024-12-23 00:41:07 +0300

    refactor and add new err types

--- src/errors/types.ts
+++ src/errors/types.ts
@@ -1,3 +1,22 @@
 export interface ErrorWithMessage {
-    message: string;
+	message: string
+}
+
+export enum RoutingErrorType {
+	CHAIN_NOT_SUPPORTED = 'CHAIN_NOT_SUPPORTED',
+	NO_ROUTE_FOUND = 'NO_ROUTE_FOUND',
+	TOKEN_NOT_SUPPORTED = 'TOKEN_NOT_SUPPORTED',
+	TOO_LOW_AMOUNT = 'TOO_LOW_AMOUNT',
+	TOO_HIGH_AMOUNT = 'TOO_HIGH_AMOUNT',
+	UNKNOWN_ERROR = 'UNKNOWN_ERROR',
+	MISSING_PARAMS = 'MISSING_PARAMS',
+	WRONG_SLIPPAGE = 'WRONG_SLIPPAGE',
+	WRONG_AMOUNT = 'WRONG_AMOUNT',
+	AMOUNT_BELOW_FEE = 'AMOUNT_BELOW_FEE',
+	SAME_TOKENS = 'SAME_TOKENS',
+}
+
+export interface RoutingErrorParams {
+	type: RoutingErrorType
+	[key: string]: any
 }
