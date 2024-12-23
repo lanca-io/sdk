85bc984d2640f9ed9c3d8b23a30d2237eae5ec4e
Aleksei Gerasev <alexey@concero.io>
2024-12-23 15:42:10 +0300

    add missing constant

--- src/constants/default.ts
+++ src/constants/default.ts
@@ -2,3 +2,4 @@ export const DEFAULT_SLIPPAGE = '0.5'
 export const DEFAULT_TOKENS_LIMIT = '10000000'
 export const DEFAULT_REQUEST_RETRY_INTERVAL_MS = 3000 // 3 seconds
 export const DEFAULT_RETRY_COUNT = 3
+export const DEFAULT_GAS_LIMIT = 3_000_000n
