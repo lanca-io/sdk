98b9469dd1446b7c7586fc9ed9b8ad687fdf1450
Aleksei Gerasev <alexey@concero.io>
2024-12-25 18:27:19 +0300

    add getPublicClient func for the future usage

---
+++ src/utils/getPublicClient.ts
@@ -0,0 +1,12 @@
+import { createPublicClient, http } from 'viem'
+import { supportedViemChainsMap } from '../configs'
+
+export function getPublicClient(chainId: string) {
+	const { chain, provider } = supportedViemChainsMap[chainId]
+	const publicClient = createPublicClient({
+		chain,
+		transport: provider ?? http(),
+	})
+
+	return publicClient
+}
