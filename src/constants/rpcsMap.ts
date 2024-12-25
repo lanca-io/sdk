ce28f1c09c4c05b686649f2fca3c154ef1cc5b5b
Aleksei Gerasev <alexey@concero.io>
2024-12-25 18:23:50 +0300

    add rpcs

---
+++ src/constants/rpcsMap.ts
@@ -0,0 +1,28 @@
+export const rpcsMap: Record<string, string[]> = {
+	'42161': [
+		'https://arbitrum.llamarpc.com',
+		'https://arbitrum-one.publicnode.com',
+		'https://rpc.ankr.com/arbitrum',
+		'https://arb1.arbitrum.io/rpc',
+		'https://arbitrum.blockpi.network/v1/rpc/public',
+	],
+	'43114': [
+		'https://avalanche-c-chain-rpc.publicnode.com',
+		'https://rpc.ankr.com/avalanche',
+		'https://avalanche.public-rpc.com',
+	],
+	'137': [
+		'https://polygon.llamarpc.com',
+		'https://polygon-bor-rpc.publicnode.com',
+		'https://polygon-rpc.com',
+		'https://rpc.ankr.com/polygon',
+	],
+	'8453': [
+		'https://base.gateway.tenderly.co',
+		'https://gateway.tenderly.co/public/base',
+		'https://base.llamarpc.com',
+		'https://base-rpc.publicnode.com',
+		'https://mainnet.base.org',
+		'https://public.stackup.sh/api/v1/node/base-mainnet',
+	],
+}
