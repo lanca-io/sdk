f77b44cedb8f3284774d827e784d3b219954974e
Aleksei Gerasev <alexey@concero.io>
2025-01-08 00:33:29 +0300

    extend eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,25 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import pluginJs from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
 
 export default [
-	{ files: ["**/*.{js,mjs,cjs,ts}"] },
+	{ files: ['**/*.{ts}'] },
 	{
 		languageOptions: { globals: { ...globals.browser, ...globals.node } },
 		rules: {
-			"@typescript-eslint/no-unused-vars": "warn",
-			"@typescript-eslint/no-explicit-any": "off",
+			'@typescript-eslint/no-unused-vars': 'warn',
+			'@typescript-eslint/no-explicit-any': 'warn',
+			'@typescript-eslint/no-empty-interface': 'warn',
+			'@typescript-eslint/naming-convention': [
+				'error',
+				{
+					selector: 'parameter',
+					format: ['camelCase', 'PascalCase', 'UPPER_CASE', 'snake_case'],
+					leadingUnderscore: 'allow',
+				},
+			],
 		},
 	},
 	pluginJs.configs.recommended,
 	...tseslint.configs.recommended,
-];
+]
