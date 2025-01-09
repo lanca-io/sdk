b70b599ae63661191b6e03140ce7657a58833e6c
Aleksei Gerasev <alexey@concero.io>
2025-01-09 18:16:03 +0300

    move magic number to constants

--- src/utils/stringifyWithBigInt.ts
+++ src/utils/stringifyWithBigInt.ts
@@ -1,3 +1,9 @@
+import { DEFAULT_INDENTATION } from '../constants'
+
 export function stringifyWithBigInt(obj: unknown): string {
-	return JSON.stringify(obj, (_, value) => (typeof value === 'bigint' ? value.toString() : value), 2)
+	return JSON.stringify(
+		obj,
+		(_, value) => (typeof value === 'bigint' ? value.toString() : value),
+		DEFAULT_INDENTATION,
+	)
 }
