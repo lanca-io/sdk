eb0a84555148efefe934a8324cb1f1ed0083c448
Aleksei Gerasev <alexey@concero.io>
2025-01-27 14:52:38 +0300

    add customHttp

---
+++ src/http/customHttp.ts
@@ -0,0 +1,32 @@
+import { Transport } from 'viem'
+
+export function createCustomHttp(url: string, options?: unknown): Transport {
+	return () => ({
+		name: `customHttp-${url}`,
+		async request({ method, params }: { method: string; params: unknown[] }) {
+			const response = await fetch(url, {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify({
+					jsonrpc: '2.0',
+					id: Date.now(),
+					method,
+					params,
+				}),
+			})
+
+			const jsonResponse = await response.json()
+
+			if (method === 'eth_getTransactionByHash' && !jsonResponse.result) {
+				throw new Error('Transaction not found')
+			}
+
+			if (!response.ok || jsonResponse.error) {
+				throw new Error(`Error from ${url}: ${jsonResponse.error?.message || response.statusText}`)
+			}
+
+			return jsonResponse.result
+		},
+		config: options,
+	})
+}
