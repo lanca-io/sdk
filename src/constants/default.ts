25fbb459957ac9acd1fe97831e363fff8e29c13e
Aleksei Gerasev <alexey@concero.io>
2025-01-29 18:43:10 +0300

    add DEFAULT_CONFIRMATIONS

--- src/constants/default.ts
+++ src/constants/default.ts
@@ -3,3 +3,4 @@ export const DEFAULT_TOKENS_LIMIT = '10000000'
 export const DEFAULT_REQUEST_RETRY_INTERVAL_MS = 3000 // 3 seconds
 export const DEFAULT_RETRY_COUNT = 3
 export const DEFAULT_INDENTATION = 2
+export const DEFAULT_CONFIRMATIONS = 3
