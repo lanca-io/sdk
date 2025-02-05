f0c3861e650c498ed26b4482adf8d3e7d8fa2761
Aleksei Gerasev <alexey@concero.io>
2025-02-05 21:38:13 +0300

    change routeStatus api

--- src/configs/apis.ts
+++ src/configs/apis.ts
@@ -1,7 +1,7 @@
 import { BASE_URL } from '../constants'
 
 export const conceroApi = {
-	routeStatus: `${BASE_URL}/route_status`,
+	routeStatus: `${BASE_URL}/route/status`,
 	route: `${BASE_URL}/route`,
 	tokens: `${BASE_URL}/tokens`,
 	chains: `${BASE_URL}/chains`,
