fbf41fbd793840ee85dd0086a0da0bc9f673451c
Aleksei Gerasev <alexey@concero.io>
2025-02-06 17:23:15 +0300

    simplify

--- src/constants/supportedChains.ts
+++ src/constants/supportedChains.ts
@@ -2,10 +2,10 @@ import { arbitrum, avalanche, base, optimism, polygon } from 'viem/chains'
 
 export const SUPPORTED_CHAINS = [arbitrum, optimism, avalanche, base, polygon]
 
-export const SUPPORTED_OP_CHAINS = {
-	[optimism.id.toString()]: true,
-	[base.id.toString()]: true,
-	[arbitrum.id.toString()]: false,
-	[avalanche.id.toString()]: false,
-	[polygon.id.toString()]: false,
+export const SUPPORTED_OP_CHAINS: Record<number, boolean> = {
+	[optimism.id]: true,
+	[base.id]: true,
+	[arbitrum.id]: false,
+	[avalanche.id]: false,
+	[polygon.id]: false,
 }
