23c7b8057878349f5e352b94a1b60750947571cc
Aleksei Gerasev <alexey@concero.io>
2025-02-11 17:48:56 +0300

    add warning if we dont have envs

--- src/constants/baseUrl.ts
+++ src/constants/baseUrl.ts
@@ -1,3 +1,9 @@
 const PRODUCTION_URL = 'https://api.concero.io/api'
 const DEBUG_URL = process.env.CONCERO_API_URL
-export const BASE_URL = process.env.DEBUG === 'true' ? DEBUG_URL : PRODUCTION_URL
+const isDebug = process.env.DEBUG?.toLowerCase() === 'true'
+
+if (isDebug && !DEBUG_URL) {
+	console.warn('⚠️  Warning: DEBUG mode is enabled, but CONCERO_API_URL is not set! Using PRODUCTION_URL.')
+}
+
+export const BASE_URL = isDebug && DEBUG_URL ? DEBUG_URL : PRODUCTION_URL
