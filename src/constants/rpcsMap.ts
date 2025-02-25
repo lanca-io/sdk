2fd0d6c541d82ae1e9919f9f1707739734118b38
babamovandrej <andrejbabamov@gmail.com>
2025-02-25 15:14:06 +0100

    Added more rpc to config

--- src/constants/rpcsMap.ts
+++ src/constants/rpcsMap.ts
@@ -1,28 +1,35 @@
 export const rpcsMap: Record<string, string[]> = {
 	'42161': [
+		'https://1rpc.io/arb',
 		'https://arbitrum-one.publicnode.com',
 		'https://rpc.ankr.com/arbitrum',
-		'https://arb1.arbitrum.io/rpc',
+		'https://endpoints.omniatech.io/v1/arbitrum/one/public',
 		'https://arbitrum.blockpi.network/v1/rpc/public',
+		'https://arb1.arbitrum.io/rpc',
 	],
 	'43114': [
+		'https://1rpc.io/avax/c',
 		'https://avalanche-c-chain-rpc.publicnode.com',
 		'https://avalanche.drpc.org',
-		'https://avax.meowrpc.com',
 		'https://rpc.ankr.com/avalanche',
 		'https://avalanche.public-rpc.com',
+		'https://api.avax.network/ext/bc/C/rpc'
 	],
 	'137': [
-		'https://polygon-bor-rpc.publicnode.com',
+		'https://polygon-pokt.nodies.app',
 		'https://rpc.ankr.com/polygon',
+		'https://polygon-bor-rpc.publicnode.com',
+		'https://polygon.drpc.org',
 		'https://polygon.rpc.subquery.network/public',
 		'https://polygon-rpc.com',
 	],
 	'8453': [
-		'https://mainnet.base.org',
+		'https://base.gateway.tenderly.co',
+		'https://base-pokt.nodies.app',
 		'https://base-rpc.publicnode.com',
 		'https://base.drpc.org',
 		'https://base.meowrpc.com',
+		'https://mainnet.base.org',
 	],
 	'10': [
 		'https://optimism-rpc.publicnode.com',
@@ -30,5 +37,6 @@ export const rpcsMap: Record<string, string[]> = {
 		'https://optimism.meowrpc.com',
 		'https://1rpc.io/op',
 		'https://endpoints.omniatech.io/v1/op/mainnet/public',
+		'https://mainnet.optimism.io',
 	],
 }
