458973fad99a7890071897e59f60e7f59a0e3c3b
babamovandrej <andrejbabamov@gmail.com>
2025-04-14 15:26:40 +0200

    Add logic for custom confirmations based on chain id

--- src/constants/viemReceiptConfig.ts
+++ src/constants/viemReceiptConfig.ts
@@ -3,6 +3,5 @@ import { DEFAULT_CONFIRMATIONS } from './default'
 
 export const viemReceiptConfig: Partial<WaitForTransactionReceiptParameters> = {
 	pollingInterval: 3_000,
-	confirmations: DEFAULT_CONFIRMATIONS,
 	timeout: 0,
 }
