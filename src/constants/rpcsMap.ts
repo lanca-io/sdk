63b31064a785f3bf26b82880f189c7e4650c781f
babamovandrej <andrejbabamov@gmail.com>
2025-04-28 12:13:41 +0200

    Update rpc list for base and op

--- src/constants/rpcsMap.ts
+++ src/constants/rpcsMap.ts
@@ -18,29 +18,16 @@ export const rpcsMap: Record<string, string[]> = {
 		'https://polygon-rpc.com',
 	],
 	'8453': [
-		'https://1rpc.io/base',
+		'https://base-pokt.nodies.app',
 		'https://base-rpc.publicnode.com',
-		'https://base.drpc.org',
+		'https://base.blockpi.network/v1/rpc/public',
+		'https://0xrpc.io/base',
 		'https://mainnet.base.org',
 	],
 	'10': [
-		'https://1rpc.io/op',
+		'https://op-pokt.nodies.app',
 		'https://optimism-rpc.publicnode.com',
-		'https://optimism.drpc.org',
 		'https://endpoints.omniatech.io/v1/op/mainnet/public',
+		'https://mainnet.optimism.io',
 	],
-	// V2
-	'421614': [
-		'https://sepolia-rollup.arbitrum.io/rpc',
-		'https://arbitrum-sepolia.gateway.tenderly.co',
-		'https://arbitrum-sepolia.drpc.org',
-	],
-	'84532': [
-		'https://base-sepolia.gateway.tenderly.co',
-		'https://base-sepolia.drpc.org',
-		'https://base-sepolia-rpc.publicnode.com',
-		'https://sepolia.base.org',
-	],
-	'2021': ['https://saigon-testnet.roninchain.com/rpc'],
-	'6342': ['https://carrot.megaeth.com/rpc'],
 }
