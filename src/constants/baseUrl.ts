2da1a36b6f3349ff63e209fe777db61d1e781a18
babamovandrej <andrejbabamov@gmail.com>
2025-04-30 17:02:06 +0200

    Update baseUrl to eradicate the process.env

--- src/constants/baseUrl.ts
+++ src/constants/baseUrl.ts
@@ -1,3 +1,1 @@
-const PRODUCTION_URL = 'https://api.concero.io/api'
-const DEBUG_URL = process.env.CONCERO_API_URL
-export const BASE_URL = process.env.DEBUG === 'true' ? DEBUG_URL : PRODUCTION_URL
+export const BASE_URL = 'https://api.concero.io/api'
