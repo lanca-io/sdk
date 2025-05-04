2ab6f410efd98b250226014099385ee5ad037e0e
babamovandrej <andrejbabamov@gmail.com>
2025-05-04 18:57:32 +0200

    Include tsup configuration in tsconfig file

--- src/constants/viemReceiptConfig.ts
+++ src/constants/viemReceiptConfig.ts
@@ -1,5 +1,4 @@
 import { WaitForTransactionReceiptParameters } from 'viem'
-import { DEFAULT_CONFIRMATIONS } from './default'
 
 export const viemReceiptConfig: Partial<WaitForTransactionReceiptParameters> = {
 	pollingInterval: 3_000,
