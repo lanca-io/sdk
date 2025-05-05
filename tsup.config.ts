632720ec6ebec10f53b8d037d2616e28bfdfaeb1
babamovandrej <andrejbabamov@gmail.com>
2025-05-05 09:23:34 +0200

    Fix tsconfig issues because of rootDir

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,21 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/index.ts'],          
-  format: ['esm', 'cjs'],           
-  dts: true,                         
-  sourcemap: true,                
-  clean: true,                      
-  minify: true,                     
+  entry: ['src/index.ts'],
+  format: ['esm', 'cjs'],
+  dts: true,
+  sourcemap: true,
+  clean: true,
+  minify: false,
   outDir: 'dist',
+  target: 'es2022',
+  platform: 'node',
+  shims: true,
+  treeshake: true,
+  outExtension({ format }) {
+    return { 
+      js: format === 'esm' ? '.mjs' : '.cjs' 
+    }
+  },
+  external: ['viem', 'pino', 'solady'], 
 });
