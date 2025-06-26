b1e237902e0d87382f9616bd0cd861b4406de906
babamovandrej <andrejbabamov@gmail.com>
2025-06-26 13:41:40 +0200

    Setup v2 of sdk

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vitest/config'
-import { TEST_TIMEOUT } from './tests/setup'
 
 export default defineConfig({
 	test: {
-		globals: true,
-		environment: 'node',
-		coverage: {
-			reporter: ['ts'],
-		},
-		setupFiles: ['./tests/setup.ts'],
-		testTimeout: TEST_TIMEOUT,
-	},
+		dangerouslyIgnoreUnhandledErrors: true
+	}
 })
