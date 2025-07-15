8980353d9a0ff36ea081ae9f87add086ab436a9c
babamovandrej <andrejbabamov@gmail.com>
2025-07-15 11:26:36 +0200

    Cache client requests

--- src/utils/getPublicClient.ts
+++ src/utils/getPublicClient.ts
@@ -1,12 +1,17 @@
+import type { PublicClient } from 'viem'
 import { createPublicClient, http } from 'viem'
 import { supportedViemChainsMap } from '../configs'
 
-export function getPublicClient(chainId: string) {
-	const { chain, provider } = supportedViemChainsMap[chainId]
-	const publicClient = createPublicClient({
-		chain,
-		transport: provider ?? http(),
-	})
+const clients: Record<string, PublicClient> = {}
 
-	return publicClient
+export function getPublicClient(chainId: string) {
+    if (clients[chainId]) {
+        return clients[chainId]
+    }
+    const { chain, provider } = supportedViemChainsMap[chainId]
+    const publicClient = createPublicClient({
+        chain,
+        transport: provider ?? http(),
+    })
+    clients[chainId] = publicClient
 }
