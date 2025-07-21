50f35c39ff66030624e817c068b7561a67638220
babamovandrej <andrejbabamov@gmail.com>
2025-07-21 10:41:19 +0200

    Add balance checks

---
+++ src/core/balances/checkBalances.ts
@@ -0,0 +1,14 @@
+import type { Client } from "viem";
+import type { Address } from "viem";
+import { getTokenBalance } from "./getBalances";
+import { getNativeBalance } from "./getBalances";
+
+export const checkNativeBalance = async (client: Client, amount: bigint): Promise<boolean> => {
+    const balance: bigint = await getNativeBalance(client);
+    return balance >= amount;
+}
+
+export const checkTokenBalance = async (client: Client, tokenAddress: Address, amount: bigint): Promise<boolean> => {
+    const balance: bigint = await getTokenBalance(client, tokenAddress);
+    return balance >= amount;
+}
---
+++ src/core/balances/getBalances.ts
@@ -0,0 +1,40 @@
+import type { Client } from "viem"
+import type { Address } from "viem"
+import { erc20Abi } from "viem"
+import { getAction } from "viem/utils"
+import { getBalance } from "viem/actions"
+import { readContract } from "viem/actions"
+
+export const getTokenBalance = async (
+    client: Client,
+    tokenAddress: Address,
+): Promise<bigint> => {
+    if (!client.account?.address) {
+        return 0n;
+    }
+    return getAction(
+        client,
+        readContract,
+        'readContract',
+    )({
+        account: client.account!,
+        abi: erc20Abi,
+        functionName: 'balanceOf',
+        args: [client.account.address],
+        address: tokenAddress,
+    })
+}
+
+export const getNativeBalance = async (client: Client): Promise<bigint> => {
+    if (!client.account?.address) {
+        return 0n;
+    }
+    
+    return getAction(
+        client,
+        getBalance,
+        'getBalance',
+    )({
+        address: client.account?.address,
+    })
+}
