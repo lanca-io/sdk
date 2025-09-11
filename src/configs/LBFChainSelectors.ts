e64fb276529dfb25fc9e039456d174482995b5c2
babamovandrej <andrejbabamov@gmail.com>
2025-09-11 16:52:54 +0200

    Integrate LBF Testnet

---
+++ src/configs/LBFChainSelectors.ts
@@ -0,0 +1,94 @@
+export const LBFChainSelectors: Record<string, bigint> = {
+	//// APECHAIN CURTIS ////
+	'33111': 33111n,
+
+	//// ARBITRUM SEPOLIA ////
+	'421614': 421614n,
+
+	//// AVALANCHE FUJI ////
+	'43113': 43113n,
+
+	//// BASE SEPOLIA ////
+	'84532': 84532n,
+
+	//// BITLAYER TESTNET ////
+	'200810': 200810n,
+
+	//// BLAST SEPOLIA ////
+	'168587773': 1685877n,
+
+	//// BNB TESTNET ////
+	'97': 97n,
+
+	//// CELO ALFAJORES ////
+	'44787': 44787n,
+
+	//// CORE TESTNET ////
+	'1114': 1114n,
+
+	//// CRONOS TESTNET ////
+	'338': 338n,
+
+	//// GNOSIS CHIADO ////
+	'10200': 10200n,
+
+	//// LINEA SEPOLIA ////
+	'59141': 59141n,
+
+	//// MONAD TESTNET ////
+	'10143': 10143n,
+
+	//// OPTIMISM SEPOLIA ////
+	'11155420': 11155420n,
+
+	//// POLYGON AMOY ////
+	'80002': 80002n,
+
+	//// SCROLL SEPOLIA ////
+	'534351': 534351n,
+
+	//// SEI TESTNET ////
+	'1328': 1328n,
+
+	//// SEPOLIA ////
+	'11155111': 11155111n,
+
+	//// SONEIUM MINATO ////
+	'1946': 1946n,
+
+	//// UNICHAIN SEPOLIA ////
+	'1301': 1301n,
+
+	//// BERACHAIN BEPOLIA ////
+	'80069': 80069n,
+
+	//// OP BNB TESTNET ////
+	'5611': 5611n,
+
+	//// AURORA TESTNET ////
+	'1313161555': 1313161n,
+
+	//// BOB SEPOLIA ////
+	'808813': 808813n,
+
+	//// FLOW TESTNET ////
+	'545': 545n,
+
+	//// METIS SEPOLIA ////
+	'59902': 59902n,
+
+	//// KAVA TESTNET ////
+	'2221': 2221n,
+
+	//// WEMIX TESTNET ////
+	'1112': 1112n,
+
+	//// IRYS TESTNET ////
+	'1270': 1270n,
+
+	//// MANTA PACIFIC SEPOLIA ////
+	'3441006': 344100n,
+
+	//// SONIC BLAZE TESTNET ////
+	'57054': 57054n,
+}
